47de237fd08991cf63352998359cabf27de74a15
Michal Lekstan <m.lekstan@interia.pl>
2024-08-16 17:40:12 +0200

    Almost nothing changed

--- py_client/simple.py
+++ py_client/simple.py
@@ -1,7 +1,7 @@
 import requests
 
-endpoint = 'http://httpbin.org/anything'
+endpoint = 'http://localhost:8000/strona'
 
 get_response = requests.get(endpoint)
 
-print(get_response.json())
+print(get_response.status_code)
